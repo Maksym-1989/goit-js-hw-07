b99c5d5ff52aaf044a23f7f70c6974fec6ffb2b2
Maksym-1989 <zaksumy1989@gmail.com>
2021-05-05 21:30:39 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,17 @@
 const inputRef = document.querySelector("input#validation-input");
 const inputDataLength = Number(inputRef.dataset.length);
+const valid = "valid";
+const error = "invalid";
+
 inputRef.addEventListener("input", (event) => {
-  const valid = "valid";
-  const error = "invalid";
   if (inputRef.value.length === inputDataLength) {
-    inputRef.classList.remove(error);
-    inputRef.classList.add(valid);
-    return;
+    updateClass(valid, error);
+  } else {
+    updateClass(error, valid);
   }
-  inputRef.classList.remove(valid);
-  inputRef.classList.add(error);
 });
+
+function updateClass(addCla, remCla) {
+  inputRef.classList.remove(remCla);
+  inputRef.classList.add(addCla);
+}
